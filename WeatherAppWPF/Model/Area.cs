d71164a1154290e3d1538d5c586144845b07e2e2
Halim Can Ocaklı <halimcanocakli@gmail.com>
2022-08-18 22:17:26 +0300

    Create Area.cs

---
+++ WeatherAppWPF/Model/Area.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherAppWPF.Model
+{
+    public class Area
+    {
+        public string ID { get; set; }
+        public string LocalizedName { get; set; }
+    }
+}
