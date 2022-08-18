12e42bea3f85c022c7f244424ad1d58dec59b974
Halim Can Ocaklı <halimcanocakli@gmail.com>
2022-08-18 22:17:24 +0300

    Create City.cs

---
+++ WeatherAppWPF/Model/City.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherAppWPF.Model
+{
+    public class City
+    {
+        public int Version { get; set; }
+        public string Key { get; set; }
+        public string Type { get; set; }
+        public int Rank { get; set; }
+        public string LocalizedName { get; set; }
+        public Area Country { get; set; }
+        public Area AdministrativeArea { get; set; }
+    }
+}
