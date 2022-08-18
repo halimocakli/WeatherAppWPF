d30b1e9b2ad2f0a835cb795b8d01564d7b1b875c
Halim Can Ocaklı <halimcanocakli@gmail.com>
2022-08-18 22:17:50 +0300

    Create SearchCommand.cs

---
+++ WeatherAppWPF/ViewModel/Commands/SearchCommand.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using WeatherAppWPF.ViewModel;
+
+namespace WeatherAppWPF.ViewModel.Commands
+{
+    public class SearchCommand : ICommand
+    {
+        public WeatherViewModel ViewModel { get; set; }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public SearchCommand(WeatherViewModel viewModel)
+        {
+            ViewModel = viewModel;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            string query = parameter as string;
+
+            if (string.IsNullOrWhiteSpace(query))
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public void Execute(object parameter)
+        {
+            ViewModel.MakeQuery();
+        }
+    }
+}
