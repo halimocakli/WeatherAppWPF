df334f08b92f1fd1dabcc18179880dd507233032
Halim Can Ocaklı <halimcanocakli@gmail.com>
2022-08-18 22:17:31 +0300

    Create Units.cs

---
+++ WeatherAppWPF/Model/Units.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherAppWPF.Model
+{
+    public class Units
+    {
+        public string Value { get; set; }
+        public string Unit { get; set; }
+        public int UnitType { get; set; }
+
+    }
+}
