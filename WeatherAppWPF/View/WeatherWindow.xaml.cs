b44cadfbfe4dce148b9e4c44f40322a7b39a38b3
Halim Can Ocaklı <halimcanocakli@gmail.com>
2022-08-18 22:17:45 +0300

    Create WeatherWindow.xaml.cs

---
+++ WeatherAppWPF/View/WeatherWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace WeatherAppWPF.View
+{
+    /// <summary>
+    /// Interaction logic for WeatherWindow.xaml
+    /// </summary>
+    public partial class WeatherWindow : Window
+    {
+        public WeatherWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
