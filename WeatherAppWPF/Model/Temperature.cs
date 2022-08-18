05a0a366f8e7bc9e49821fd77e584debd84ba27a
Halim Can Ocaklı <halimcanocakli@gmail.com>
2022-08-18 22:17:29 +0300

    Create Temperature.cs

---
+++ WeatherAppWPF/Model/Temperature.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherAppWPF.Model
+{
+    public class Temperature
+    {
+        public Units Metric { get; set; }
+        public Units Imperial { get; set; }
+    }
+}
